5cfedaf104990d742de224dbcaa26ec6da2d62c2
pandey_divyansh <divyanshpandey@3396@gmail.com>
2022-05-19 14:10:34 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import {Route,Routes,BrowserRouter} from 'react-router-dom'
+import HomePage from './pages/HomePage';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path='/home' element={<HomePage/>}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
